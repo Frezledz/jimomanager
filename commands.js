4f386533c16d085212a517d59bba9f6be515dbbf
xxXFreezerXxx <freezerfx512@gmail.com>
2023-01-26 09:17:07 +0000

    d

--- commands.js
+++
@@ -1,5 +0,0 @@
-const {SlashCommandBuilder} = require("@discordjs/builders");
-const commands = [
-    (new SlashCommandBuilder().setName("ping").setDescription("Return With pong.")).toJSON(),
-    (new SlashCommandBuilder().setName("auth").setDescription("認証を開始します。認証チャンネルでのみ利用可能です。")).toJSON(),
-];
