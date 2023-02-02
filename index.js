0ac053c2d29f09d2f025a54aef008ba1783dd54c
xxxfreezerxxx <freezerfx512@gmail.com>
2023-02-02 07:35:47 +0000

    a

--- index.js
+++ index.js
@@ -120,7 +120,7 @@ if(interaction.customId==="auth"){
                 let parsed = JSON.parse(rawdata);
                 const userid = interaction.user.id.toString();
                 let rank;
-                axios({url: `https://jimoapi.glitch.me/api/user/${scratchname}`,method:"get"}).then(res=>{
+                getrank(scratchname).then(res=>{
                   rank=res.data;
                   return;
                 }).catch(()=>{
@@ -216,3 +216,35 @@ http.createServer(function(req, res) {
   res.write("online");
   res.end();
 }).listen(8080);
+
+const getrank = (username)=>{
+  return new Promise((resolve,reject)=>{
+      axios({url: `https://api.scratch.mit.edu/studios/29958336`,method:"get"}).then(res=>res.data).catch(res=>reject(res)).then(res=>{
+          const raw = res.description.split("\n");
+          let i=0;
+          let ii=0;
+          let needstr =[[ "• – • Master intro makers • – •","• – • Advanced intro makers • – •","• – • Good intro makers • – •"],[null,"Master","Advanced","Good"]];
+          while(true){
+              
+              if(i>raw.length-1){
+                  resolve("visitor");
+                  break;
+              }
+              const tmp =raw[i].toLowerCase();
+              if(ii<3){
+                  if(tmp.includes(needstr[0][ii].toLowerCase())){
+                      ii++;
+                  }
+              }
+              if(tmp.includes(username.toLowerCase())&&ii!=0){
+                  resolve(needstr[1][ii]);
+                  break;
+                  
+              }
+              i++;
+          }
+
+      });
+
+  })
+};
